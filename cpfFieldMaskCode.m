af5b1fb5b8444676f29258a217b5e045422aa8e4
joaopedronardari <joaonardari@terra.com.br>
2014-05-03 13:08:07 -0300

    Adding code cpfMask .m
    This code goes into your UIViewController.m

---
+++ cpfFieldMaskCode.m
@@ -0,0 +1,23 @@
+/**
+*   CODE BY Joao Pedro Nardari dos Santos - joaonardari@terra.com.br
+*   Add the code in your UIViewController.m and set cpfField Outset in your UIViewController.h
+*   and @syntesize cpfField
+*/
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    if (textField == cpfField) {
+        if ([string length] == 0)
+            return YES;
+        
+        if (textField.text.length > 13)
+            return NO;
+        
+        if ([textField.text length] == 3 || [textField.text length] == 7) {
+            textField.text = [textField.text stringByAppendingString:@"."];
+        } else if ([textField.text length] == 11) {
+            textField.text = [textField.text stringByAppendingString:@"-"];
+        }
+    }
+    
+    return YES;
+}
